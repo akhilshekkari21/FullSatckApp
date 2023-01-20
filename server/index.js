8a859183211d75815bf6c290012f8d29a88a9f9d
akhilshekkari21 <54269083+akhilshekkari21@users.noreply.github.com>
2023-01-20 22:26:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({limit: "30mb",extended : true}));
 app.use(bodyParser.urlencoded({limit: "30mb",extended : true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://akhil:Techno%40GG21@cluster0.q3jrnfo.mongodb.net/?retryWrites=true&w=majority';
+
 
 const port = process.env.PORT || 5000;
 mongoose.set('strictQuery', true);
